c65295f8447b0337abe2812eb75b3e91ac554bad
Simon Guschlbauer <S1610307066@students.fh-hagenberg.at>
2019-01-10 14:21:36 +0100

    Fixed Presentation of active element in HeaderComponent

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
 export class HeaderComponent implements OnInit {
-  isNavbarCollapsed=true;
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
