9e5432905336f10620853a8728974c71a1d9add1
Simon Guschlbauer <S1610307066@students.fh-hagenberg.at>
2019-01-12 00:43:41 +0100

    Fixed Autocomplete and Filtering of SearchComponent

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Station } from '../api/models/station';
-import { switchMap, distinctUntilChanged, debounceTime, tap } from 'rxjs/operators';
 import { StationService } from '../api/services';
 
 @Component({
@@ -9,8 +8,10 @@ import { StationService } from '../api/services';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-  isLoading = false;
   foundStations: Station[] = [];
+  stations: Station[] = [];
+  value: string;
+
   @Output() stationSelected = new EventEmitter<Station>();
 
   private keyup = new EventEmitter<String>();
@@ -18,15 +19,23 @@ export class SearchComponent implements OnInit {
   constructor(private stationService: StationService) { }
 
   ngOnInit() {
-    this.keyup.pipe(
-      debounceTime(500),
-      distinctUntilChanged(),
-      tap(() => this.isLoading = true),
-      switchMap(searchTerm => this.stationService.StationGetLikeStationName(searchTerm.toString())), // toString is used to avoid a unexplainable type mismatch between String and string
-      tap(() => this.isLoading = false)
-    ).subscribe(stations => this.foundStations = stations);
+    this.stationService.StationGetAllStations()
+      .subscribe(stations => this.stations = stations);
+  }
 
+  filterStations(searchTerm: string) {
+    if (searchTerm == "") {
+      this.foundStations = [];
+      return;
+    }
     
+    searchTerm = searchTerm.charAt(0).toUpperCase() + searchTerm.slice(1);
+    this.foundStations = this.stations.filter(station => station.Name.startsWith(searchTerm));
+  }
+
+  stationClicked(station: Station) {
+    this.value = "";
+    this.foundStations = [];
   }
 
   
